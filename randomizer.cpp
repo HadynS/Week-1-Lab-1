fb16ca247990a7dc34070170472c85e9b5398cf3
Charlesvlutz <cvlutz42@tntech.edu>
2024-09-18 15:26:55 -0500

    Add randomizer and names

---
+++ randomizer.cpp
@@ -0,0 +1,46 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+    vector<string> names;
+    string filename = "names.txt";
+    ifstream file(filename);
+    string name;
+
+    // Check if the file opens successfully
+    if (!file) 
+    {
+        cout << "Error opening file: " << filename << endl;
+        return 1;
+    }
+
+    // Read names from the file and store them in the vector
+    while (getline(file, name)) 
+    {
+        if (!name.empty()) 
+        {
+            names.push_back(name);
+        }
+    }
+
+    file.close();
+
+    // Seed the random number generator
+    srand(time(0));
+
+    // Select a random index from the vector
+    int winnerIndex = rand() % names.size();
+
+    // Print the winner
+    cout << "The winner of the raffle is: " << names[winnerIndex] << "!" << endl;
+
+    return 0;
+}
