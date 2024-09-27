1a257428d0c01725ef743cc35cda3208cd0be916
bhuddlez2 <rbhuddlest42@tntech.edu>
2024-09-27 12:55:32 -0500

    Started on raffle storage class

--- RaffleStorage.cpp
+++ RaffleStorage.cpp
@@ -3,3 +3,56 @@
 //Read in names from txt files
 //method to print names to the screen
 //destructor to free memory from pointers
+
+#include "RaffleStorage.h"
+#include "People.h"
+#include "RaffleEntry.h"
+
+RaffleTicket** RaffleTicket::tickets = nullptr;
+int RaffleTicket::ticketCount;
+int RaffleTicket::capacity;
+
+//Constructor
+RaffleTicket::RaffleTicket(int id, People* name, PersonalInfo* info)
+    : ticketID(id), participantName(name),participantInfo(info) {}
+
+RaffleTicket::~RaffleTicket()
+{
+    delete participantName;
+    delete participantInfo;
+}
+
+int RaffleTicket::getTicketID()
+{
+    return ticketID;
+}
+
+People* RaffleTicket::getParticipantName()
+{
+    return participantName;
+}
+
+PersonalInfo* RaffleTicket::getParticipantInfo() 
+{
+    return participantInfo;
+}
+
+void RaffleTicket::printTicketDetails() {
+    cout << "----- Raffle Ticket Details -----" << endl;
+    cout << "Ticket ID: " << ticketID << endl;
+    //cout << "Participant Name: " << participantName->getName() << endl; //-from people file
+    //cout << "Participant Address: " << participantInfo->getAddress() << endl; //-from entry file
+    //cout << "Participant Phone Number: " << participantInfo->getNumber() << endl;  //-from entry file
+    cout << "---------------------------------" << endl;
+}
+
+void RaffleTicket::addTicket(RaffleTicket* ticket)
+{
+    //code for adding ticket
+}
+
+void RaffleTicket::loadFromFile(string& filename)
+{
+    //code for loading from txt file
+}
+
--- RaffleStorage.h
+++ RaffleStorage.h
@@ -4,8 +4,44 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
+//forward declaration
+class People;
+class PersonalInfo; 
+
+class RaffleTicket 
+{
+private:
+
+    int ticketID;
+    People* participantName;
+    PersonalInfo* participantInfo;
+
+public:
+
+    static RaffleTicket** tickets;
+    static int ticketCount; 
+    static int capacity;
+
+    //Constructor 
+    RaffleTicket(int id, People* name, PersonalInfo* info); 
+
+    //Destructor
+    ~RaffleTicket();
+
+    //Accessor methods
+    int getTicketID();
+    People* getParticipantName();
+    PersonalInfo* getParticipantInfo();
+    void printTicketDetails(); //prints tickets
+    void addTicket(RaffleTicket* ticket);//add tickets
+    void loadFromFile(string& filename); // loads all tickets from txt file
+    
 
 
 
+
+};
 
 #endif
