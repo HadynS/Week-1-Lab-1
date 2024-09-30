cbc2992e1ba4e93714393d1ac1e3746a07c5e853
HadynS <hcsimmons43@tntech.edu>
2024-09-30 14:15:53 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,41 @@
+#include "RaffleSystem.h"
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    RaffleSystem raffleSystem;
+    int choice;
+
+    raffleSystem.loadParticipants();
+
+    do 
+    {
+        cout << "Menu :\n";
+        cout << "1. Add a participant (up to 3)\n";
+        cout << "2. Draw a winner\n";
+        cout << "3. Print all participants\n";
+        cout << "4. End Program\n";
+        cin >> choice;
+
+        switch (choice) 
+        {
+            case 1: 
+                raffleSystem.addParticipant();
+                break;
+            case 2:
+                raffleSystem.drawWinner();
+                break;
+            case 3:
+                raffleSystem.printParticipant();
+                break;
+            case 4:
+                cout << "Exiting program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 4);
+    return 0;
+}
